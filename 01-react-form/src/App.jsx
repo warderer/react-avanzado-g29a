c8661ed9dadafc010d02f4a84393bdc54c7bb0c4
CesarDev96 <cesar@crecy.io>
2024-02-27 21:17:10 -0600

    Creacion del componente hook form

--- 01-react-form/src/App.jsx
+++ 01-react-form/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import SimpleForm from './components/SimpleForm'
+import ReactHookForm from './components/ReactHookForm'
 
 function App() {
   return (
     <>
-      <SimpleForm />
+      {/* <SimpleForm /> */}
+      <ReactHookForm />
     </>
   )
 }
