254e770047a2ff95aea045364066bd6cea5463dd
César Guerra <warderer@gmail.com>
2024-02-29 21:27:01 -0600

    ✨ Implementación del Contexto

--- 02-react-context/src/context/SongContext.jsx
+++ 02-react-context/src/context/SongContext.jsx
@@ -1,6 +1,10 @@
-import { createContext } from 'react'
+import { createContext, useState, useEffect } from 'react'
 // CONTEXT tiene que ver con el manejo de estados globales en REACT.
 // Es decir, poder compartir LA MISMA información entre diferentes NIVELES de componentes.
+
+// Usamos la @ para no tener que poner ../../
+// import canciones from '../../assets/listaCanciones.json'
+import canciones from '@/assets/listaCanciones.json'
 
 // Para usar Context, necesitamos seguir una serie de pasos.
 
@@ -11,9 +15,21 @@ const SongContext = createContext()
 // El provider es un componente que envuelve a los componentes que necesitan acceder a la información del contexto. El provider maneja donde se obtiene la información y como se comparte.
 
 function SongProvider ({ children }) {
+  const [list, setList] = useState([]) // Lista de canciones
+  const [loading, setLoading] = useState(true) // ¿Esta cargando?
+  const [selectedSong, setSelectedSong] = useState({}) // Canción seleccionada
+
+  // Simulo la llamada a la API
+  useEffect(() => {
+    setTimeout(() => {
+      setList(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   return (
-    // El provider recibe una prop llamada value, que es la información que queremos compartir.
-    <SongContext.Provider value={{}}>
+  // El provider recibe una prop llamada value, que es la información que queremos compartir.
+    <SongContext.Provider value={{ list, loading, selectedSong, setSelectedSong }}>
       {children}
     </SongContext.Provider>
   )
---
+++ 02-react-context/src/hooks/useSongContext.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import { SongContext } from '@/context/SongContext'
+
+// Paso 3. Crear un custom hook para consumir el contexto
+export const useSongContext = () => {
+  const context = useContext(SongContext)
+
+  if (!context) {
+    throw new Error('useSongContext debe ser usado dentro del SongProvider')
+  }
+
+  return context
+}
