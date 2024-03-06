b9d1c31303fb86fcd7114f90ebf6cabe0a94f9b4
CesarDev96 <cesar@crecy.io>
2024-03-05 21:09:03 -0600

    Creacion de archivo scss

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 import { Home, Dashboard, Login, Secret, Signup } from '@/pages'
 
 function App() {
 
   return (
     <>
-      <h1>React Auth</h1>
+      <Header />
       <Home />
       <Dashboard />
       <Login />
