0ef71842abf4a8c7d0f88a287767a2422548c5bc
CesarDev96 <cesar@crecy.io>
2024-03-05 20:58:10 -0600

    Creacion de las paginas y instalacion de dependencias

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { Home, Dashboard, Login, Secret, Signup } from '@/pages'
 
 function App() {
 
   return (
     <>
       <h1>React Auth</h1>
+      <Home />
+      <Dashboard />
+      <Login />
+      <Secret />
+      <Signup />
     </>
   )
 }
---
+++ 03-react-auth/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from '@/pages/Home'
+import Dashboard from '@/pages/Dashboard'
+import Login from '@/pages/Login'
+import Secret from '@/pages/Secret'
+import Signup from '@/pages/Signup'
+
+export { Home, Dashboard, Login, Secret, Signup }
