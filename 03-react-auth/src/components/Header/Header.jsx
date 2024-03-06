aae47a8c82f86fcc2f768210b8b4f5c3d219de11
CesarDev96 <cesar@crecy.io>
2024-03-05 21:38:00 -0600

    Implementacion de estilos scss

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -2,7 +2,26 @@ import './header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='header'>
+      <a className='header__logo' href='/'>Logo</a>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/home'>Home</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/dashboard'>Dashboard</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/login'>Login</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/secret'>Secret</a>
+        </li>
+        <li className='header__list-item'>
+          <a className='header__item-link' href='/signup'>Signup</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
