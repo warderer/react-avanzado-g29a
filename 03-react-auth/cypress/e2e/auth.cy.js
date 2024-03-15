cd53529f0dca035ebe64b4bcc985b24ecdf9a18d
César Guerra <warderer@gmail.com>
2024-03-14 21:00:51 -0600

    ✅ Test: Login como Customer

--- 03-react-auth/cypress/e2e/auth.cy.js
+++ 03-react-auth/cypress/e2e/auth.cy.js
@@ -7,4 +7,16 @@ describe('Funcionalidad de Login', () => {
     cy.get('h1')
       .contains('Home') // 03. Assert: Verificar que la App se comporta como esperamos
   })
+
+  it('Probar que funciona Login como CUSTOMER', () => {
+    // 01. Arrange: Setup del estado de mi App
+    cy.visit('/login')
+
+    // 02. Act: Ejecutar acciones / interactuar con la App
+    cy.get('input[type="email"]').type('drstrange@marvel.com')
+    cy.get('input[type="password"]').type('multiverso')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('h1').contains('Dashboard') // 03. Assert: Verificar que la App se comporta como esperamos
+  })
 })
