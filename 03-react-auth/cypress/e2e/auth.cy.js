ec7eaf9ebcf443ba7f99c658ba6302b9378faf21
César Guerra <warderer@gmail.com>
2024-03-14 21:13:23 -0600

    ✅ Test: Logout como Admin

--- 03-react-auth/cypress/e2e/auth.cy.js
+++ 03-react-auth/cypress/e2e/auth.cy.js
@@ -24,4 +24,25 @@ describe('Funcionalidad de Login', () => {
 
     cy.get('h1').contains('Dashboard') // 03. Assert: Verificar que la App se comporta como esperamos
   })
+
+  it('Cuando haga Logout como ADMIN me lleve a la página de Home', () => {
+    // Intercep lo uso para saber cuando una petición fue realizada y puedo esperarla con wait()
+    cy.intercept('POST', 'https://ecommerce-json-jwt.onrender.com/login').as('login')
+
+    // 01. Arrange: Setup del estado de mi App
+    cy.visit('/login')
+
+    // 02. Act: Ejecutar acciones / interactuar con la App
+    cy.get('input[type="email"]').type('superman@dc.com')
+    cy.get('input[type="password"]').type('superman')
+    cy.get('button[type="submit"]').click()
+
+    cy.wait('@login') // Esperar a que la petición de login sea realizada
+
+    // La logica para hacer logout
+    // cy.get('nav > ul li:last').click()
+    cy.get('a').contains('Logout').click()
+
+    cy.get('h1').contains('Home') // 03. Assert: Verificar que la App se comporta como esperamos
+  })
 })
